c25692b29da2179c16ca89039548cc942df01651
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2023-11-03 15:25:09 +0100

    fix: validate wasmFileUrl

--- src/crypto.ts
+++ src/crypto.ts
@@ -54,6 +54,10 @@ export async function loadRifRollupCrypto(wasmFileUrl?: string) {
     if (rifRollupCryptoLoaded) {
         return;
     }
+
+    if (wasmFileUrl && !validateWasmFileUrl(wasmFileUrl)) {
+        throw new Error('Invalid wasmFileUrl');
+    }
     // Only runs in the browser
     const _rifRollupCrypto = rifRollupCrypto as any;
     if (_rifRollupCrypto.loadZkSyncCrypto) {
@@ -71,3 +75,54 @@ export async function loadRifRollupCrypto(wasmFileUrl?: string) {
         rifRollupCryptoLoaded = true;
     }
 }
+
+const validateWasmFileUrl = (wasmFileUrl: string) => {
+    // Regular expression pattern to match a valid URL format
+    const urlPattern = new RegExp(
+      '^' +
+        // protocol identifier (optional)
+        // short syntax // or long syntax http:// or https://
+        '(?:(?:(?:https?|ftp):)?\\/\\/)' +
+        // user:pass BasicAuth (optional)
+        '(?:\\S+(?::\\S*)?@)?' +
+        '(?:' +
+        // IP address exclusion
+        // private & local networks
+        // private IP ranges: 10.0.0.0 - 10.255.255.255, 172.16.0.0 - 172.31.255.255, 192.168.0.0 - 192.168.255.255
+        '(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
+        // IP address dotted notation octets
+        // excludes loopback network 0.0.0.0
+        // excludes reserved space >= 224.0.0.0
+        // excludes network & broadcast addresses
+        // (first & last IP address of each class)
+        '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
+        '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
+        '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
+        '|' +
+        // host & domain names, may end with dot
+        // can be replaced by a shortest alternative
+        '(?:' +
+        '(?:' +
+        '[a-z0-9\\u00a1-\\uffff]' +
+        '[a-z0-9\\u00a1-\\uffff_-]{0,62}' +
+        ')?' +
+        '[a-z0-9\\u00a1-\\uffff]\\.' +
+        ')+' +
+        // TLD identifier name, may end with dot
+        '(?:[a-z\\u00a1-\\uffff]{2,}\\.?)' +
+        ')' +
+        // port number (optional)
+        '(?::\\d{2,5})?' +
+        // resource path (optional)
+        '(?:[/?#]\\S*)?' +
+        '$',
+      'i'
+    );
+  
+    // Check if the string matches the URL pattern and does not contain executable code
+    if (urlPattern.test(wasmFileUrl) && !wasmFileUrl.includes('javascript:')) {
+      return true; // Valid URL without potentially malicious code
+    } else {
+      return false; // Invalid URL or potentially malicious code found
+    }
+  }
